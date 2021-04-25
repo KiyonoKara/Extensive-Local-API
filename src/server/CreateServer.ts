325b4e8e8c943e45c2d6e5fbbc733f951f1ddbc4
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-25 16:50:59 -0700

    Create server file

---
+++ src/server/CreateServer.ts
@@ -0,0 +1,16 @@
+import * as http from "http";
+import { setHeaders } from "../util/Headers";
+import { STATUS_OK } from "../util/constants/StatusPayloads";
+import "../util/StringFabrication";
+
+export const server: http.Server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
+    if (request.url === "/") {
+        setHeaders(response,
+            {
+                "content-type": "text/plain; charset=utf-8",
+                "date": new Date().toUTCString()
+            });
+        response.writeHead(response.statusCode, response.statusMessage);
+        response.end(STATUS_OK.stringify(), 'utf-8');
+    }
+});
