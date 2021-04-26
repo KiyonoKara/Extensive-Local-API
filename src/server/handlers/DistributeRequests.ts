d93a52df86d233150ed4c81a0127a486ccc7f1c4
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-25 17:27:24 -0700

    Calls the API file which holds all of the endpoints and API components

---
+++ src/server/handlers/DistributeRequests.ts
@@ -0,0 +1,12 @@
+import * as fs from "fs";
+import { API } from "./API";
+
+export const distribute = (request, response, options?) => {
+    fs.readdirSync('src/api')
+        .forEach(file => {
+            if (file.endsWith(".ts")) {
+                const component: string = file.split('.').slice(0, -1).join('.');
+                API[component].run(request, response, options ? options : {});
+            }
+    });
+};
