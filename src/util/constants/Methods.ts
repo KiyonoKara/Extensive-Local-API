2864bebb607a1220d8f8b0ec79d9d6780a68a3ff
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-25 20:06:09 -0700

    Request method constants

---
+++ src/util/constants/Methods.ts
@@ -0,0 +1,11 @@
+const
+    GET: string = "GET",
+    POST: string = "POST",
+    DELETE: string = "DELETE",
+    PUT: string = "PUT",
+    PATCH: string = "PATCH",
+    HEAD: string = "HEAD";
+
+export {
+    GET, POST, DELETE, PUT, PATCH, HEAD
+};
