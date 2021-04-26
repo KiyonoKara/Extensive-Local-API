f6a0f80bf8c19e233d718343a1e056ad62c39c1c
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-25 21:07:12 -0700

    Indexing constants into one neat file

---
+++ src/util/constants/IndexConstants.ts
@@ -0,0 +1,11 @@
+import * as Functions from "./Functions";
+import * as Methods from "./Methods";
+import * as Specifications from "./Specifications";
+import * as StatusPayloads from "./StatusPayloads";
+
+export {
+    Functions,
+    Methods,
+    Specifications,
+    StatusPayloads
+};
