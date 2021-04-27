31067e97df113175bd46580eeddf90bfec3bc27c
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-27 15:02:17 -0700

    Add another endpoint to API.ts

--- src/server/handlers/API.ts
+++ src/server/handlers/API.ts
@@ -1,8 +1,13 @@
 import * as Home from "../../api/Home";
+import * as Echo from "../../api/Echo";
 
 export const API = {
     Home: {
         main: Home,
         path: "/"
+    },
+    Echo: {
+        main: Echo,
+        path: "/echo"
     }
 };
