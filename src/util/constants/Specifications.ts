5735a2d72eb1ec9d337ec2b419ec7d55138875b1
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-27 10:49:07 -0700

    More constants which are for content types and dates

--- src/util/constants/Specifications.ts
+++ src/util/constants/Specifications.ts
@@ -1,5 +1,9 @@
 const DEFAULT_ENCODING: string = "utf-8";
+const APPLICATION_JSON_CT: string = "application/json; charset=utf-8";
+const CURRENT_DATE: string = new Date().toUTCString()
 
 export {
-    DEFAULT_ENCODING
+    DEFAULT_ENCODING,
+    APPLICATION_JSON_CT,
+    CURRENT_DATE
 };
