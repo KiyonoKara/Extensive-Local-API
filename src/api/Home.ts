eaa669681ae698a3c4df4293ab71538fb57b30ec
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-26 18:21:38 -0700

    Improved responses

--- src/api/Home.ts
+++ src/api/Home.ts
@@ -3,10 +3,11 @@ import { STATUS_OK } from "../util/constants/StatusPayloads";
 import "../util/StringFabrication";
 
 export const run = (request, response, options) => {
+    console.log(options);
     if (request.url === "/") {
         setHeaders(response,
             {
-                "content-type": "text/plain; charset=utf-8",
+                "content-type": "application/json; charset=utf-8",
                 "date": new Date().toUTCString()
             });
         response.writeHead(response.statusCode, response.statusMessage);
