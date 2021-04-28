fb6f6f62bfcce1d2185f00fbbd0a78b69d12390c
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-28 09:32:43 -0700

    Default headers added

--- src/server/handlers/DistributeRequests.ts
+++ src/server/handlers/DistributeRequests.ts
@@ -2,9 +2,22 @@ import * as fs from "fs";
 import { API } from "./API";
 import * as Utilities from "../../util/IndexUtil";
 import { KeyObject } from "../../util/Interfaces";
+import { setHeaders } from "../../util/Headers";
+import * as Methods from "../../util/constants/Methods";
+import * as Specifications from "../../util/constants/Specifications";
 
 export const distribute = (request, response, options: KeyObject = {}) => {
+    // Setting options just in case
     options.utilities = Utilities;
+    // Setting default headers
+    setHeaders(response,
+        {
+            "allow": `${Methods.GET}, ${Methods.POST}, ${Methods.DELETE}, ${Methods.PUT}, ${Methods.PATCH}, ${Methods.HEAD}`,
+            "date": Specifications.CURRENT_DATE,
+            "cache-control": Specifications.NO_CACHE,
+            "content-encoding": `${Specifications.BR}`
+        });
+
     fs.readdirSync('src/api')
         .forEach(file => {
             if (file.endsWith(".ts")) {
