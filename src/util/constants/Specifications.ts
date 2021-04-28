b1eddf00bd97666876bd0aca956c427bd0b61ee2
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-28 09:31:48 -0700

    More specifications

--- src/util/constants/Specifications.ts
+++ src/util/constants/Specifications.ts
@@ -2,10 +2,18 @@ const DEFAULT_ENCODING: string = "utf-8";
 const APPLICATION_JSON_CT: string = "application/json; charset=utf-8";
 const TEXT_PLAIN_CT: string = "text/plain; charset=utf-8";
 const CURRENT_DATE: string = new Date().toUTCString()
+const NO_CACHE: string = "no-cache";
+const GZIP: string = "gzip";
+const DEFLATE: string = "deflate";
+const BR: string = "br";
 
 export {
     DEFAULT_ENCODING,
     APPLICATION_JSON_CT,
     TEXT_PLAIN_CT,
-    CURRENT_DATE
+    CURRENT_DATE,
+    NO_CACHE,
+    GZIP,
+    DEFLATE,
+    BR
 };
