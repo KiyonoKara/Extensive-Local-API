6b23afce4d6c6c9979304ced45d2aad317303f96
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-27 20:13:41 -0700

    Another constant to the specifications

--- src/util/constants/Specifications.ts
+++ src/util/constants/Specifications.ts
@@ -1,9 +1,11 @@
 const DEFAULT_ENCODING: string = "utf-8";
 const APPLICATION_JSON_CT: string = "application/json; charset=utf-8";
+const TEXT_PLAIN_CT: string = "text/plain; charset=utf-8";
 const CURRENT_DATE: string = new Date().toUTCString()
 
 export {
     DEFAULT_ENCODING,
     APPLICATION_JSON_CT,
+    TEXT_PLAIN_CT,
     CURRENT_DATE
 };
