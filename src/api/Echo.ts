9d7b7e23ddec9404187bed036063bdc3847ce4da
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-27 20:14:18 -0700

    Echo endpoint has been completed? (No)

--- src/api/Echo.ts
+++ src/api/Echo.ts
@@ -1,7 +1,25 @@
 import * as HandleMethods from "../server/handlers/HandleMethods";
+import * as Methods from "../util/constants/Methods";
+import { setHeaders } from "../util/Headers";
+import { Constants } from "../util/IndexUtil";
 
 export const run = (request, response, options) => {
     if (options.path === "/echo") {
-        HandleMethods.handleGET(request, response, "GET", { data: { success: "Request was successful" }});
+        setHeaders(response,
+            {
+                "Allow": `${Methods.GET}, ${Methods.POST}, ${Methods.DELETE}, ${Methods.PUT}, ${Methods.PATCH}, ${Methods.HEAD}`,
+                "date": Constants.Specifications.CURRENT_DATE
+            });
+        if (request.method === Methods.POST || request.method === Methods.PUT || request.method === Methods.PATCH) {
+            HandleMethods.writtenBody(request, response, request.method, { isJSON: true }).then(data => {
+                if (data['message']) {
+                    HandleMethods.sendWrittenOutput(request, response, request.method, { data: { success: `${request.method} request was successful.`, message: data['message'] }, isJSON: true });
+                }
+            });
+        } else if(request.method === "HEAD") {
+            HandleMethods.handleHEAD(request, response, request.method);
+        } else {
+            HandleMethods.handleMethod(request, response, request.method, { data: { success: `${request.method} request was successful.` }, isJSON: true });
+        }
     }
 }
