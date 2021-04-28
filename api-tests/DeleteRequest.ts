f83b6fc654448a4244738bd612acc2cf282d2afb
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-28 09:32:55 -0700

    Delete request tests

---
+++ api-tests/DeleteRequest.ts
@@ -0,0 +1,14 @@
+import * as http from "http";
+
+const request = http.request({
+    hostname: "localhost",
+    port: 8080,
+    method: "DELETE",
+    path: "/echo"
+}, callback => {
+    let body = [] as Buffer[];
+    callback.on('data', chunk => body.push(chunk));
+    callback.on('end', () => console.log(Buffer.concat(body).toString()));
+});
+
+request.end();
